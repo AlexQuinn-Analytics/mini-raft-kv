f8af5af2851c82ceb110f703365b4f2e933d18f5
AlexQuinn--SDE <1527962551@qq.com>
2026-06-14 18:35:12 +0800

    Refactor Node class to include gRPC server functionality

--- src/main/java/com/miniraftkv/node/Node.java
+++ src/main/java/com/miniraftkv/node/Node.java
@@ -1,27 +1,68 @@
 package com.miniraftkv.node;
+
+import java.io.IOException;
+
+import com.miniraftkv.rpc.AppendEntriesResponse;
+import com.miniraftkv.rpc.RaftClient;
+import com.miniraftkv.rpc.RaftServiceImpl;
+import com.miniraftkv.rpc.RequestVoteResponse;
 import com.miniraftkv.state.NodeState;
 
-public class Node{
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class Node {
     private String nodeId;
     private long currentTerm;
     private NodeState state;
+    private int port;
+    private Server server;
+
     
-    Node(String nodeId){
-        this.nodeId=nodeId;
-        this.currentTerm=0;
-        this.state=NodeState.FOLLOWER;
+    public Node(String nodeId, int port) {
+        this.nodeId = nodeId;
+        this.port = port;
+        this.currentTerm = 0;
+        this.state = NodeState.FOLLOWER;
     }
     
-    public void printStatus(){
+    public void printStatus() {
         System.out.println("Node:" + nodeId + ", Term:" + currentTerm + ", State:" + state);
     }
+    public void start() throws IOException{
+        server =  ServerBuilder.forPort(port)
+        .addService(new RaftServiceImpl(nodeId))
+        .build()
+        .start();
+        System.out.println("[" + nodeId +"] gRPC Server started, listening on port " + port);
+    }
+    public void blockUntilShutdown() throws InterruptedException{
+        if (server!=null){
+            server.awaitTermination();
+        }
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
     
-    public static void main(String[]args){
-        Node node1=new Node("Node1");
-        Node node2=new Node("Node2");
-        Node node3=new Node("Node3");
-        node1.printStatus();
-        node2.printStatus();
-        node3.printStatus();
+    Node node1 = new Node("Node1", 5001);
+    node1.printStatus();
+    node1.start();
+
+    Thread.sleep(1000);
+        
+    RaftClient client = new RaftClient("localhost", 5001);
+
+    System.out.println("\n--- Testing RequestVote ---");
+    RequestVoteResponse voteResponse = client.sendRequestVote("Node2", 1);
+    System.out.println("Got vote response: term=" + voteResponse.getTerm() 
+        + ", voteGranted=" + voteResponse.getVoteGranted());
+
+    System.out.println("\n--- Testing AppendEntries ---");
+    AppendEntriesResponse appendResponse = client.sendAppendEntries("Node2", 1);
+    System.out.println("Got append response: term=" + appendResponse.getTerm() 
+        + ", success=" + appendResponse.getSuccess());
+
+    client.shutdown();
+
+    node1.blockUntilShutdown();
     }
 }
