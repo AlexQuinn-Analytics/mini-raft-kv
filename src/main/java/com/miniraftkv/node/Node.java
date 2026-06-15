552032fec513348565944b8ac16c26e5ae78421e
AlexQuinn--SDE <1527962551@qq.com>
2026-06-15 20:54:39 +0800

    Remove unused List import from Node.java
    Removed unused import for List.

--- src/main/java/com/miniraftkv/node/Node.java
+++ src/main/java/com/miniraftkv/node/Node.java
@@ -2,7 +2,6 @@ package com.miniraftkv.node;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
