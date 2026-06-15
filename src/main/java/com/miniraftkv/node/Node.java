85888f269185e6c59117e5db2971432c5d0e095d
AlexQuinn--SDE <1527962551@qq.com>
2026-06-15 20:48:28 +0800

    Add peer list and election handling to Node class

--- src/main/java/com/miniraftkv/node/Node.java
+++ src/main/java/com/miniraftkv/node/Node.java
@@ -1,8 +1,14 @@
 package com.miniraftkv.node;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-import com.miniraftkv.rpc.AppendEntriesResponse;
 import com.miniraftkv.rpc.RaftClient;
 import com.miniraftkv.rpc.RaftServiceImpl;
 import com.miniraftkv.rpc.RequestVoteResponse;
@@ -17,18 +23,28 @@ public class Node {
     private NodeState state;
     private int port;
     private Server server;
+    private String votedFor;
+    private ScheduledExecutorService scheduler;
+    private ScheduledFuture<?> electionTimer;
+    private final Random random = new Random();
+    private List<String> peers;
+    private int voteCount;
 
     
-    public Node(String nodeId, int port) {
+    public Node(String nodeId, int port, List<String> peers) {
         this.nodeId = nodeId;
         this.port = port;
         this.currentTerm = 0;
+        this.peers = peers;
         this.state = NodeState.FOLLOWER;
+        this.votedFor = null;
+        this.scheduler = Executors.newScheduledThreadPool(1);
     }
     
     public void printStatus() {
         System.out.println("Node:" + nodeId + ", Term:" + currentTerm + ", State:" + state);
     }
+
     public void start() throws IOException{
         server =  ServerBuilder.forPort(port)
         .addService(new RaftServiceImpl(nodeId))
@@ -36,6 +52,50 @@ public class Node {
         .start();
         System.out.println("[" + nodeId +"] gRPC Server started, listening on port " + port);
     }
+
+    public void resetElectionTimer(){
+        if (electionTimer!=null){
+            electionTimer.cancel(false);
+        }
+        int timeout = 800 + random.nextInt(800);
+        electionTimer = scheduler.schedule(
+            ()->startElection(),
+            timeout,
+            TimeUnit.MILLISECONDS
+        );
+    }
+
+    public void startElection(){
+        state = NodeState.CANDIDATE;
+        currentTerm++;
+        votedFor = nodeId;
+        voteCount = 1;
+        System.out.println("[" + nodeId + "] Became CANDIDATE, term =" + currentTerm + ", requesting votes from peers...");
+        for (String peer:peers){
+            requestVoteFromPeer(peer);
+        }
+        resetElectionTimer();
+
+    }
+
+    public void requestVoteFromPeer(String peer){
+        try{
+            String[] parts = peer.split(":");
+            String host = parts[0];
+            int peerPort = Integer.parseInt(parts[1]);
+            RaftClient client = new RaftClient(host, peerPort);
+            RequestVoteResponse response= client.sendRequestVote(nodeId, currentTerm);
+            if (response.getVoteGranted()){
+                voteCount++;
+                System.out.println("[" + nodeId + "] Got vote from " + peer 
+                + ", total votes: " + voteCount);
+            }
+            client.shutdown();
+        }catch(Exception e){
+            System.out.println("[" + nodeId + "] Failed to contact " + peer);
+        }
+    }
+
     public void blockUntilShutdown() throws InterruptedException{
         if (server!=null){
             server.awaitTermination();
