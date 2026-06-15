7675053892ac274037388347d0ae5b0e1c334047
AlexQuinn--SDE <1527962551@qq.com>
2026-06-15 20:54:50 +0800

    Add List import to Node.java

--- src/main/java/com/miniraftkv/node/Node.java
+++ src/main/java/com/miniraftkv/node/Node.java
@@ -3,6 +3,7 @@ package com.miniraftkv.node;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
