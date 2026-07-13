5e9ade1ecab777f83d6bc83a4380c442053a2965
AlexQuinn--SDE <1527962551@qq.com>
2026-07-13 22:34:46 +0800

    Add flush method to SSTable for data persistence
    Implemented flush method to write data to SSTable file.

--- src/main/java/com/miniraftkv/storage/SSTable.java
+++ src/main/java/com/miniraftkv/storage/SSTable.java
@@ -12,3 +12,14 @@ public class SSTable {
     public SSTable(String filename) {
         this.filename = filename;
     }
+    
+    public void flush(Map<String, String> data) {
+        try {
+            FileWriter writer = new FileWriter(filename);
+            for (Map.Entry<String, String> entry : data.entrySet()) {
+                writer.write(entry.getKey() + "=" + entry.getValue() + "\n");
+            }
+            writer.close()
+            System.out.println("[SSTable] Flushed " + data.size() 
+                + " entries to " + filename);
+        }
