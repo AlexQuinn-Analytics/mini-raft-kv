03b0fe8bb7a45c51e36663a6f4c652609d6f4419
AlexQuinn--SDE <1527962551@qq.com>
2026-07-16 23:21:15 +0800

    Add LSMStore class with initial fields

--- src/main/java/com/miniraftkv/storage/LSMStore.java
+++ src/main/java/com/miniraftkv/storage/LSMStore.java
@@ -5,3 +5,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+
+public class LSMStore {
+    private MemTable memTable;
+    private List<SSTable> ssTables;
+    private int maxMemTableSize;
+    private int ssTableCounter = 0;
